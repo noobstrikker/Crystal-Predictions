75a144fc5d3e889f76095ed34e2d0386b9e891ca
nicol <nicolai.nicolai.berwald@gmail.com>
2025-02-21 17:31:44 +0100

    un-test but i think finalized apidownloader and loader

--- CFILoader.py
+++ CFILoader.py
@@ -1,4 +1,4 @@
-#get class in this line
+import crystalclass
 def ReadFileToComma(*file):
     currectword = ""
     while(True):
@@ -18,17 +18,36 @@ def LoadFromDisc(filename, amount):
     for x in range(amount): # its from to 0 to "> amount"
         
         material_id = ReadFileToComma(*file) #only string rest is floats, do some covertation
-        currectword = ""
-        for y in range(30):
-            currectword = ""
-        while(True):
-            cl =file.read(1)
-            if cl == ",":
-                file.read(1)#skips space after ,
-                break
-            else:
-                currectword += cl
-        
+        band_gap = float(ReadFileToComma(*file))
+        cbm = float(ReadFileToComma(*file))
+        density = float(ReadFileToComma(*file))
+        density_atomic = float(ReadFileToComma(*file))
+        dos_energy_down = float(ReadFileToComma(*file))
+        dos_energy_up = float(ReadFileToComma(*file))
+        e_electronic = float(ReadFileToComma(*file))
+        e_ij_max = float(ReadFileToComma(*file))
+        e_ionic = float(ReadFileToComma(*file))
+        e_total = float(ReadFileToComma(*file))
+        efermi = float(ReadFileToComma(*file))
+        energy_above_hull = float(ReadFileToComma(*file))
+        energy_per_atom = float(ReadFileToComma(*file))
+        equilibrium_reaction_energy_per_atom = float(ReadFileToComma(*file))
+        formation_energy_per_atom = float(ReadFileToComma(*file))
+        homogeneous_poisson = float(ReadFileToComma(*file))
+        n = float(ReadFileToComma(*file))
+        shape_factor = float(ReadFileToComma(*file))
+        surface_anisotropy = float(ReadFileToComma(*file))
+        total_magnetization = float(ReadFileToComma(*file))
+        total_magnetization_normalized_formula_units = float(ReadFileToComma(*file))
+        total_magnetization_normalized_vol = float(ReadFileToComma(*file))
+        uncorrected_energy_per_atom = float(ReadFileToComma(*file))
+        universal_anisotropy = float(ReadFileToComma(*file))
+        vbm = float(ReadFileToComma(*file))
+        volume = float(ReadFileToComma(*file))
+        weighted_surface_energy = float(ReadFileToComma(*file))
+        weighted_surface_energy_EV_PER_ANG2 = float(ReadFileToComma(*file))
+        weighted_work_function = float(ReadFileToComma(*file))
+
     
     materials.append(crystalclass(material_id,band_gap, cbm, density, density_atomic, dos_energy_down, dos_energy_up, e_electronic,
                     e_ij_max, e_ionic, e_total, efermi, energy_above_hull, energy_per_atom, equilibrium_reaction_energy_per_atom,
--- crystalclass.py
+++ crystalclass.py
@@ -20,3 +20,18 @@ class crystal:
         self.energy_per_aton = energy_per_atom
         self.equilibrium_reaction_energy_per_atom = equilibrium_reaction_energy_per_atom
         self.formation_energy_per_atom = formation_energy_per_atom
+        self.homogeneous_poisson = homogeneous_poisson
+        self.n = n
+        self.shape_factor = shape_factor
+        self.surface_anisotropy = surface_anisotropy
+        self.total_magnetization = total_magnetization
+        self.total_magnetization_normalized_formula_units = total_magnetization_normalized_formula_units
+        self.total_magnetization_normalized_vol = total_magnetization_normalized_vol
+        self.uncorrected_energy_per_atom = uncorrected_energy_per_atom
+        self.universal_anisotropy = universal_anisotropy
+        self.vbm = vbm
+        self.volume = volume
+        self.weighted_surface_energy = weighted_surface_energy
+        self.weighted_surface_energy_EV_PER_ANG2 = weighted_surface_energy_EV_PER_ANG2
+        self.weighted_work_function = weighted_work_function
+
