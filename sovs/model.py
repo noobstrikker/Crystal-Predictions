2e8d1583dc50fe3950fc64d10966aef9dcf6b350
bobber200 <hejbobber@live.dk>
2025-03-25 10:21:36 +0100

    model.py

---
+++ sovs/model.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+
+class CrystalGNN(nn.Module):
+    def __init__(self, num_features, hidden_channels=64):
+        super(CrystalGNN, self).__init__()
+        self.conv = GCNConv(num_features, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+
+        #Connected layers
+        self.fc = nn.Linear(hidden_channels, 2)  
+        
+    def forward(self, x, edge_index):
+        x = self.conv(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, p=0.5, training=self.training)
+        
+        # Pooling layer
+        x = torch.mean(x, dim=0).unsqueeze(0)
+        
+        # Classification
+        x = self.fc(x)
+        return F.log_softmax(x, dim=1)
