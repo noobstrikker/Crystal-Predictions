3afc2c0f2bead3e0111c4f4a40152cd3e081c783
Kristoffer Højmark Jensen <hejbobber@live.dk>
2025-03-25 10:19:18 +0100

    train.py

---
+++ GNN/model.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch_geometric
+
+
+class GNN(nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super(GNN, self).__init__()
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, out_channels)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        x = F.relu(self.conv1(x, edge_index))
+        x = self.conv2(x, edge_index)
+        return x
